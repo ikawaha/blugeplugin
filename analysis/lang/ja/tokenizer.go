e062afdd51aff787ebdf79f30640a3c0467f097b
ikawaha <ikawaha@users.noreply.github.com>
2020-12-30 11:02:36 +0900

    Fix a tokenizer and filters

--- analysis/lang/ja/tokenizer.go
+++ analysis/lang/ja/tokenizer.go
@@ -10,53 +10,76 @@ import (
 	"github.com/ikawaha/kagome/v2/tokenizer"
 )
 
-type JapaneseTokenizer struct{
-	filter *filter.POSFilter
+var DefaultInflected = filter.NewPOSFilter(
+	filter.POS{"動詞"},
+	filter.POS{"形容詞"},
+	filter.POS{"形容動詞"},
+)
+
+type TokenizerOption func(t *JapaneseTokenizer)
+
+func StopTagsFilter() TokenizerOption {
+	return func(t *JapaneseTokenizer) {
+		tags := StopTags()
+		ps := make([]filter.POS, 0, len(tags))
+		for k := range tags {
+			ps = append(ps, strings.Split(k, "-"))
+		}
+		t.stopTagFilter = filter.NewPOSFilter(ps...)
+	}
 }
 
-func (t *JapaneseTokenizer) Tokenize(input []byte) analysis.TokenStream{
-	tnz, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
-	if err != nil {
-		panic(err)
+func BaseFormFilter() TokenizerOption {
+	return func(t *JapaneseTokenizer) {
+		t.baseFormFilter = DefaultInflected
 	}
-	tokens := tnz.Analyze(*(*string)(unsafe.Pointer(&input)), tokenizer.Search)
+}
+
+type JapaneseTokenizer struct {
+	*tokenizer.Tokenizer
+	stopTagFilter  *filter.POSFilter
+	baseFormFilter *filter.POSFilter
+}
+
+func (t *JapaneseTokenizer) Tokenize(input []byte) analysis.TokenStream {
+	tokens := t.Analyze(*(*string)(unsafe.Pointer(&input)), tokenizer.Search)
+	t.stopTagFilter.Drop(&tokens)
 	ret := make(analysis.TokenStream, 0, len(tokens))
-	for _, v := range tokens {
-		start:= v.Position
-		end := v.Position+len(v.Surface)
+	for i, v := range tokens {
+		start := v.Position
+		end := v.Position + len(v.Surface)
+		term := input[start:end]
+		if pos := v.POS(); DefaultInflected.Match(pos) {
+			if base, ok := v.BaseForm(); ok {
+				term = []byte(base)
+			}
+		}
+		incr := 1
+		if i > 0 {
+			incr = v.Index - tokens[i-1].Index
+		}
 		ret = append(ret, &analysis.Token{
 			Start:        start,
 			End:          end,
-			Term:         input[start:end],
-			PositionIncr: 1,
+			Term:         term,
+			PositionIncr: incr,
 			Type:         analysis.Ideographic,
 			KeyWord:      false,
 		})
 	}
-	return t.stopTagsFilter(ret, tokens)
+	return ret
 }
 
-func Tokenizer(stopTags analysis.TokenMap) analysis.Tokenizer {
-	ps := make([]filter.POS, 0, len(stopTags))
-	for k := range stopTags {
-		ps = append(ps, strings.Split(k, "-"))
+func Tokenizer(opts ...TokenizerOption) analysis.Tokenizer {
+	t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+	if err != nil {
+		panic(err)
 	}
-	return &JapaneseTokenizer{
-		filter: filter.NewPOSFilter(ps...),
+	ret := &JapaneseTokenizer{
+		Tokenizer: t,
 	}
-}
-
-func (t JapaneseTokenizer) stopTagsFilter(input analysis.TokenStream, tokens []tokenizer.Token) analysis.TokenStream {
-	var tail, skipped int
-	for i, v := range input {
-		if t.filter.Match(tokens[i].POS()) {
-			skipped += v.PositionIncr
-		} else {
-			v.PositionIncr += skipped
-			skipped = 0
-			input[tail] = v
-			tail++
-		}
+	for _, opt := range opts {
+		opt(ret)
 	}
-	return input[:tail]
+	return ret
 }
